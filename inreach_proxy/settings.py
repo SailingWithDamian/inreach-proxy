087d209ee758b5234f171425f3ba7dfcf75be0f1
Damian Zaremba <damian@damianzaremba.co.uk>
2025-08-26 20:12:45 +0000

    settings - cleanup default apps

--- inreach_proxy/settings.py
+++ inreach_proxy/settings.py
@@ -6,44 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = secrets.token_urlsafe()
 DEBUG = True
-ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
     "inreach_proxy",
-]
-
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
-
-ROOT_URLCONF = "inreach_proxy.urls"
-
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-            ],
-        },
-    },
 ]
 
 LOGGING = {
@@ -62,8 +27,6 @@ LOGGING = {
         "": {"handlers": ["console"], "level": "INFO", "propagate": True, "stream": sys.stdout},
     },
 }
-
-WSGI_APPLICATION = "inreach_proxy.wsgi.application"
 
 DATABASES = {
     "default": {
